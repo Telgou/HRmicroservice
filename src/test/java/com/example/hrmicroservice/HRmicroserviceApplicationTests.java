cb59601b2f41b3b5393b79ebf45e69f39a0e8687
Telgou <119911609+Telgou@users.noreply.github.com>
2023-11-26 22:39:05 +0100

    .

---
+++ src/test/java/com/example/hrmicroservice/HRmicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hrmicroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HRmicroserviceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
